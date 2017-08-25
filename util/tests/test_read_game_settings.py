70d9c45bde59b9dd3f965c9b845734faef37bb6f
yamasakih <yamasaki.sns@gmail.com>
2017-08-25 09:28:20 +0900

    Create game setting file with toml format and test it.

--- util/tests/test_read_game_settings.py
+++ util/tests/test_read_game_settings.py
@@ -1,14 +1,28 @@
 import yaml
 import os
 import sys
+import toml
 
 if __name__ == '__main__':
     libdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     if os.path.exists(libdir):
         sys.path.insert(0, libdir)
-    input_path = os.path.join(sys.path[2], 'haggle', 'game_settings', 'card_haggle_1.yaml')
-    f = open(input_path, 'r')
-    data = yaml.load(f)
+
+    print('using yaml')
+    yaml_input_path = os.path.join(sys.path[2], 'haggle', 'game_settings', 'card_haggle_1.yaml')
+    yaml_file = open(yaml_input_path, 'r')
+    data = yaml.load(yaml_file)
+    print(data)
+    for key, value in data.items():
+        print(key, value)
+
+    print('')
+
+    print('using toml')
+    toml_input_path = os.path.join(sys.path[2], 'haggle', 'game_settings', 'card_haggle_1.toml')
+    #toml_file = open(toml_input_path, 'r')
+    #data = toml.load(toml_file)
+    data = toml.load(toml_input_path)
     print(data)
     for key, value in data.items():
         print(key, value)
